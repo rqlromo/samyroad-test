c20c3dd2c5e9d425ced47c416847c36f2821bc36
Raquel Romo <rqlromo@gmail.com>
2019-09-14 19:02:29 +0200

    add card component

---
+++ src/components/card.js
@@ -0,0 +1,27 @@
+// Dependencies
+import React from "react";
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://samyroad.com/wp-content/uploads/casestudy_bbva_featured-1024x1024.jpg"
+            alt="product"
+          />
+        </div>
+        <div className="card__footer">
+          <p className="card__title">COLLECTION</p>
+          <p className="card__subtitle">
+            <span className="card__subtitle_light">by </span>underground
+          </p>
+        </div>
+        <div className="card__like"></div>
+      </div>
+    );
+  }
+}
+
+export default Card;
--- src/components/cardList.js
+++ src/components/cardList.js
@@ -1,10 +1,18 @@
 // Dependencies
 import React from "react";
 
+// Components
+import Card from "./card";
+
 class cardList extends React.Component {
   render() {
     return (
       <main className="cardList">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </main>
     );
   }
