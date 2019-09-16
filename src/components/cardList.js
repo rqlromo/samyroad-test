abaa51fdaec43d591e3237b453e9964264517f7b
Raquel Romo <rqlromo@gmail.com>
2019-09-16 19:55:33 +0200

    transform to function component

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -7,17 +7,15 @@ import Card from "./card";
 // Helpers
 import { fake_api } from "../helpers/api";
 
-class cardList extends React.Component {
-  render() {
-    return (
-      <main className="cardList">
-        {fake_api.data.length > 0 &&
-          fake_api.data.map((item, index) => {
-            return <Card key={index} item={item} />;
-          })}
-      </main>
-    );
-  }
-}
+const cardList = () => {
+  return (
+    <main className="cardList">
+      {fake_api.data.length > 0 &&
+        fake_api.data.map((item, index) => {
+          return <Card key={index} item={item} />;
+        })}
+    </main>
+  );
+};
 
 export default cardList;
