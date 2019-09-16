1571d4d479d128ddb307d0eab61e622fce3a118b
Raquel Romo <rqlromo@gmail.com>
2019-09-16 23:03:10 +0200

    transform to function component

--- src/components/card.js
+++ src/components/card.js
@@ -5,31 +5,25 @@ import PropTypes from "prop-types";
 // Components
 import Like from "../components/like";
 
-class Card extends React.Component {
+const Card = ({ item }) => {
+  const { author, title, main_attachment, likes_count } = item;
 
-  render() {    
-    const { author, title, main_attachment, likes_count } = this.props.item;
-    
-    return (
-      <div className="card">
-        <div className="card__content">
-          <img
-            className="card__image"
-            src={main_attachment.big}
-            alt="product"
-          />
-        </div>
-        <div className="card__footer">
-          <p className="card__title">{title}</p>
-          <p className="card__subtitle">
-            <span className="card__subtitle_light">by </span>{author}
-          </p>
-        </div>
-        <Like likesCount={likes_count}/>
+  return (
+    <div className="card">
+      <div className="card__content">
+        <img className="card__image" src={main_attachment.big} alt="product" />
       </div>
-    );
-  }
-}
+      <div className="card__footer">
+        <p className="card__title">{title}</p>
+        <p className="card__subtitle">
+          <span className="card__subtitle_light">by </span>
+          {author}
+        </p>
+      </div>
+      <Like likesCount={likes_count} />
+    </div>
+  );
+};
 
 export default Card;
 
