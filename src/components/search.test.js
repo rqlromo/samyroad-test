803e426c1c56d5075955c0b54556d362e75a58eb
Raquel Romo <rqlromo@gmail.com>
2019-09-17 22:52:28 +0200

    add test to search

--- src/components/search.test.js
+++ src/components/search.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import Search from "./search.js";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
+import sinon from "sinon";
 
 describe("Search", () => {
   test("renders properly", () => {
@@ -17,5 +18,14 @@ describe("Search", () => {
     const wrapper = shallow(<Search />);
 
     expect(wrapper.find('input[type="text"]').length).toEqual(1);
+  });
+
+  test("on input change run onChangeAction prop", () => {
+    const spy = sinon.spy();
+    const wrapper = shallow(<Search onChangeAction={spy} />);
+
+    wrapper.find(".searchForm__input").simulate("change");
+
+    expect(spy.calledOnce).toBe(true);
   });
 });
