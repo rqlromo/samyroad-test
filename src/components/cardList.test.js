cbac0f5c2eeabd2da17bc9160e48b9db723c914b
Raquel Romo <rqlromo@gmail.com>
2019-09-17 20:39:00 +0200

    remove unnecessary dependencies chai and chaiEnzyme

--- src/components/cardList.test.js
+++ src/components/cardList.test.js
@@ -2,16 +2,11 @@ import React from "react";
 import CardList from "./cardList.js";
 import Card from "./card.js";
 import { mount } from "enzyme";
-import chai from "chai";
-import chaiEnzyme from "chai-enzyme";
 import { cardMock } from "./mocks/mocks.js";
-
-chai.use(chaiEnzyme());
 
 describe("CardList", () => {
   test("renders properly", () => {
     const wrapper = mount(<CardList filteredData={[cardMock, cardMock]} />);
-    console.log("wrapper", wrapper);
 
     expect(wrapper.find(Card).length).toBe(2);
   });
