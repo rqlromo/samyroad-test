11455e57eb767615903d2d322ba3c2b71d6e952c
Raquel Romo <rqlromo@gmail.com>
2019-09-17 21:25:14 +0200

    add test to search

--- src/components/search.test.js
+++ src/components/search.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Search from "./search.js";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 describe("Search", () => {
   test("renders properly", () => {
@@ -10,5 +11,11 @@ describe("Search", () => {
     let tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  test("has an input", () => {
+    const wrapper = shallow(<Search />);
+
+    expect(wrapper.find('input[type="text"]').length).toEqual(1);
   });
 });
