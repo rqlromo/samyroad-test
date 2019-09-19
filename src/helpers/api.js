6eefa18237f15361c6655bcc3913a11321085bf5
Raquel Romo <rqlromo@gmail.com>
2019-09-19 21:25:03 +0200

    comment fake api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,270 +1,270 @@
 
-// Images
-import image1 from "../images/recurso1.png";
-import image2 from "../images/recurso2.png";
-import image3 from "../images/recurso3.png";
-import image4 from "../images/recurso4.png";
-import image5 from "../images/recurso5.png";
-import image6 from "../images/recurso6.png";
-import image7 from "../images/recurso7.png";
-import image8 from "../images/recurso8.png";
-import image9 from "../images/recurso9.png";
-import image10 from "../images/recurso10.png";
+// // Images
+// import image1 from "../images/recurso1.png";
+// import image2 from "../images/recurso2.png";
+// import image3 from "../images/recurso3.png";
+// import image4 from "../images/recurso4.png";
+// import image5 from "../images/recurso5.png";
+// import image6 from "../images/recurso6.png";
+// import image7 from "../images/recurso7.png";
+// import image8 from "../images/recurso8.png";
+// import image9 from "../images/recurso9.png";
+// import image10 from "../images/recurso10.png";
 
 
-export const fake_api = {
-  data: [
-    {
-      type: "Image",
-      id: 1,
-      title: "Cool hat",
-      author: "stylefresh",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image1,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 5,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },
-    {
-      type: "Image",
-      id: 2,
-      title: "Art book",
-      author: "brush",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image2,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 5,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },
-    {
-      type: "Image",
-      id: 3,
-      title: "Print modern",
-      author: "piclovers",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image3,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 5,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },
-    {
-      type: "Image",
-      id: 4,
-      title: "collection",
-      author: "underground",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image4,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 5,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },
-    {
-      type: "Image",
-      id: 5,
-      title: "Since 1.543",
-      author: "easycover",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image5,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 7,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },
-    {
-      type: "Image",
-      id: 6,
-      title: "T-shirt white",
-      author: "dulceida",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image6,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 26,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },
-    {
-      type: "Image",
-      id: 7,
-      title: "table of work",
-      author: "moretalent",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image7,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 5,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },    {
-      type: "Image",
-      id: 8,
-      title: "t-shirt red",
-      author: "m_onday",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image8,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 5,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },    {
-      type: "Image",
-      id: 9,
-      title: "cereal magazine #58",
-      author: "cerealmagazine",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image9,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 5,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    },
-    {
-      type: "Image",
-      id: 10,
-      title: "Wooden stairs",
-      author: "archit_c",
-      created_at: "2012-12-12T21: 08: 20Z",
-      main_attachment: {
-        big: image10,
-        small: "http://lorempixel.com/100/125/"
-      },
-      likes_count: 5,
-      liked: true,
-      links: [
-        {
-          rel: "avatar",
-          uri: "http://lorempixel.com/250/250/",
-          methods: "GET"
-        },
-        {
-          rel: "like",
-          uri: "http://*********.com/images/2-train-in-india-a-gra/like",
-          methods: "POST"
-        }
-      ]
-    }
-  ],
-  pagination: {
-    next: "https://*********.com/images.json?&page=2",
-    previous: null
-  }
-};
+// export const fake_api = {
+//   data: [
+//     {
+//       type: "Image",
+//       id: 1,
+//       title: "Cool hat",
+//       author: "stylefresh",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image1,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 5,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },
+//     {
+//       type: "Image",
+//       id: 2,
+//       title: "Art book",
+//       author: "brush",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image2,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 5,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },
+//     {
+//       type: "Image",
+//       id: 3,
+//       title: "Print modern",
+//       author: "piclovers",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image3,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 5,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },
+//     {
+//       type: "Image",
+//       id: 4,
+//       title: "collection",
+//       author: "underground",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image4,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 5,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },
+//     {
+//       type: "Image",
+//       id: 5,
+//       title: "Since 1.543",
+//       author: "easycover",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image5,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 7,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },
+//     {
+//       type: "Image",
+//       id: 6,
+//       title: "T-shirt white",
+//       author: "dulceida",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image6,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 26,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },
+//     {
+//       type: "Image",
+//       id: 7,
+//       title: "table of work",
+//       author: "moretalent",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image7,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 5,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },    {
+//       type: "Image",
+//       id: 8,
+//       title: "t-shirt red",
+//       author: "m_onday",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image8,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 5,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },    {
+//       type: "Image",
+//       id: 9,
+//       title: "cereal magazine #58",
+//       author: "cerealmagazine",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image9,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 5,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     },
+//     {
+//       type: "Image",
+//       id: 10,
+//       title: "Wooden stairs",
+//       author: "archit_c",
+//       created_at: "2012-12-12T21: 08: 20Z",
+//       main_attachment: {
+//         big: image10,
+//         small: "http://lorempixel.com/100/125/"
+//       },
+//       likes_count: 5,
+//       liked: true,
+//       links: [
+//         {
+//           rel: "avatar",
+//           uri: "http://lorempixel.com/250/250/",
+//           methods: "GET"
+//         },
+//         {
+//           rel: "like",
+//           uri: "http://*********.com/images/2-train-in-india-a-gra/like",
+//           methods: "POST"
+//         }
+//       ]
+//     }
+//   ],
+//   pagination: {
+//     next: "https://*********.com/images.json?&page=2",
+//     previous: null
+//   }
+// };
