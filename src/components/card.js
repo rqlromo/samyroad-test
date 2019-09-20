49bda2e4d9a5819d9a912f8961f930a73385f5ec
Raquel Romo <rqlromo@gmail.com>
2019-09-20 20:46:16 +0200

    add images from design

--- src/components/card.js
+++ src/components/card.js
@@ -2,18 +2,54 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import imagen from "../images/recurso4.png";
-
 // Components
 import Like from "../components/like";
 
-const Card = ({ item }) => {
-  const { author, title, main_attachment, liked, likes_count } = item;
+// Images
+import image1 from "../images/recurso1.png";
+import image2 from "../images/recurso2.png";
+import image3 from "../images/recurso3.png";
+import image4 from "../images/recurso4.png";
+import image5 from "../images/recurso5.png";
+import image6 from "../images/recurso6.png";
+import image7 from "../images/recurso7.png";
+import image8 from "../images/recurso8.png";
+import image9 from "../images/recurso9.png";
+import image10 from "../images/recurso10.png";
+
+const Card = ({ item, image }) => {
+  const { author, title, liked, likes_count } = item;
 
   return (
     <div className="card">
       <div className="card__content">
-        <img className="card__image" src={imagen} alt="product" />
+        <img
+          className="card__image"
+          src={
+            image === "image1"
+              ? image1
+              : image === "image2"
+              ? image2
+              : image === "image3"
+              ? image3
+              : image === "image4"
+              ? image4
+              : image === "image5"
+              ? image5
+              : image === "image6"
+              ? image6
+              : image === "image7"
+              ? image7
+              : image === "image8"
+              ? image8
+              : image === "image9"
+              ? image9
+              : image === "image10"
+              ? image10
+              : ""
+          }
+          alt="product"
+        />
       </div>
       <div className="card__footer">
         <p className="card__title">{title}</p>
--- src/components/cardList.js
+++ src/components/cardList.js
@@ -5,13 +5,12 @@ import PropTypes from "prop-types";
 // Components
 import Card from "./card";
 
-
-const CardList = ({filteredData}) => {
+const CardList = ({ filteredData }) => {
   return (
     <main className="cardList">
       {filteredData.length > 0 &&
         filteredData.map((item, index) => {
-          return <Card key={index} item={item} />;
+          return <Card key={index} item={item} image={`image${item.id}`} />;
         })}
     </main>
   );
