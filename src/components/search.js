488621e1933e3bd115bd9c1e106c91677571a9e0
Raquel Romo <rqlromo@gmail.com>
2019-09-21 11:42:47 +0200

    add focus to input

--- src/components/search.js
+++ src/components/search.js
@@ -1,14 +1,16 @@
 // Dependencies
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 // icons
 import IconSearch from "../components/icons/iconSearch";
 
 const Search = ({ withIcon, onChangeAction, onKeyDownAction }) => {
+  const [focus, setFocus] = useState(false);
+
   return (
     <form className="searchForm">
-      <div className="searchForm__content">
+      <div className={`searchForm__content ${focus ? "focus" : ""}`}>
         {withIcon && (
           <div className="searchForm__icon">
             <IconSearch width="17" height="19" />
@@ -20,6 +22,8 @@ const Search = ({ withIcon, onChangeAction, onKeyDownAction }) => {
           placeholder="You're looking for something?"
           onChange={e => onChangeAction(e.target.value)}
           onKeyDown={e => onKeyDownAction(e)}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
         />
       </div>
     </form>
